0883594e53865332cdd62226c850da5b326c88c6
Brandon Lozada <brandon_lozada@outlook.com>
2023-05-30 11:29:38 -0600

    Aplicar el estilo base

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,28 @@
 <template>
-  Brand Todo
+  <div class="brand-todo">
+    <div class="title has-text-centered">
+      Brand Todo
+    </div>
+    <div class="field is-grouped">
+    <p class="control is-expanded">
+      <input class="input" type="text" placeholder="Add a todo">
+    </p>
+    <p class="control">
+      <a class="button is-info">
+        Add
+      </a>
+    </p>
+  </div>
+  </div>
+
 </template>
+
+<style>
+@import 'node_modules\bulma\css\bulma.min.css';
+
+.brand-todo {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
