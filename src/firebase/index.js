902c8420b1c33b82838e3324e6dbb181ccd1b67c
Brandon Lozada <brandon_lozada@outlook.com>
2023-05-31 11:44:15 -0600

    Configurar db

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: 'AIzaSyCmka81vnr_e6dSKQAYZ2W1NqT8wRUfJyA',
@@ -10,3 +11,6 @@ const firebaseConfig = {
 }
 
 const app = initializeApp(firebaseConfig)
+const db = getFirestore(app);
+
+export { db }
