ce8788f63bb21ddcf0687482bafa2d00b6e7a80a
Brandon Lozada <brandon_lozada@outlook.com>
2023-05-31 11:07:48 -0600

    Agregar la funcionalidad "toggleDone"

--- src/App.vue
+++ src/App.vue
@@ -26,15 +26,27 @@
     </form>
 
 
-    <div v-for="todo in todos" class="card mb-5">
+    <div
+      v-for="todo in todos"
+      class="card mb-5"
+      :class="{ 'has-background-success-light' : todo.done }"
+    >
       <div class="card-content">
         <div class="content">
           <div class="columns is-mobile is-vcentered">
-            <div class="column">
+            <div
+              class="column"
+              :class="{ 'has-text-success line-through' : todo.done }"
+            >
               {{ todo.content }}
             </div>
             <div class="column is-5 has-text-right">
-              <button class="button is-light">
+              <button 
+                @click="toggleDone(todo.id)"
+                class="button"
+                :class="todo.done ? 'is-success' : 
+                'is-light'"
+              >
                 &check;
               </button>
               <button 
@@ -61,16 +73,16 @@ import { v4 as uuidv4 } from 'uuid';
   todos
 */
 const todos = ref([
-  {
-    id: "id1",
-    content: "Rasurar mi trasero",
-    done: false,
-  },
-  {
-    id: "id2",
-    content: "Rasurar la barba",
-    done: false,
-  }
+  // {
+  //   id: "id1",
+  //   content: "Rasurar mi trasero",
+  //   done: false,
+  // },
+  // {
+  //   id: "id2",
+  //   content: "Rasurar la barba",
+  //   done: true,
+  // }
 ])
 
 /*
@@ -90,11 +102,20 @@ const addTodo = () => {
 }
 
 /*
-deleteTodo
+  deleteTodo
 */
 
 const deleteTodo = id => {
   todos.value = todos.value.filter(todo => todo.id !== id)
+}
+
+/*
+  toggleDone
+*/
+
+const toggleDone = id => {
+  const index = todos.value.findIndex(todo => todo.id === id)
+  todos.value[index].done = !todos.value[index].done
 }
 
 </script>
@@ -106,5 +127,9 @@ const deleteTodo = id => {
   max-width: 400px;
   padding: 20px;
   margin: 0 auto;
+}
+
+.line-through {
+  text-decoration: line-through;
 }
 </style>
